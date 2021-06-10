2439f1ed350b5e1123050a8e43ecca320dd39b51
Afshar07 <mr.afshar0@gmail.com>
2021-06-11 00:35:26 +0430

    Create AvailableJobs.vue component and add company images

---
+++ src/components/AvailableJobs.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <img :src="imgLinkNew" alt="" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["logo"],
+  data() {
+    return {
+      imgLinkNew: require(`../assets/${this.logo}`),
+    };
+  },
+};
+</script>
+
+<style scoped>
+@media only screen and (max-width: 500px) {
+  .container {
+    width: 80%;
+    height: 100%;
+    background-color: #fff;
+  }
+}
+</style>
