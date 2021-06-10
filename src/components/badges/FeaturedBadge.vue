72eb60020c5c76a80764cbc16c5bd34d6485fbdc
Afshar07 <mr.afshar0@gmail.com>
2021-06-11 01:06:54 +0430

    Create and style FeaturedBadge.vue component

---
+++ src/components/badges/FeaturedBadge.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="badge-container">
+    <p>FEATURED</p>
+  </div>
+</template>
+
+<style scoped>
+.badge-container {
+  width: auto;
+  height: auto;
+  background-color: #2c3a3a;
+  border-radius: 20px;
+  margin: 1rem;
+
+}
+p {
+  color: #fff;
+  font-weight: 700;
+  font-size: 0.5rem;
+  margin: 0.3rem 0.5rem;
+}
+</style>
