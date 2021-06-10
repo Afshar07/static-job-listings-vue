68b9ce1064998188dcbcd2e3532a431844e97eb2
Afshar07 <mr.afshar0@gmail.com>
2021-06-11 00:35:56 +0430

    Manage props for available-jobs and style the component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,27 @@
 <template>
   <the-header></the-header>
+  <div class="container">
+    <available-jobs
+      v-for="data in allData"
+      :key="data.id"
+      :id="data.id"
+      :company="data.company"
+      :logo="data.logo"
+    ></available-jobs>
+  </div>
 </template>
 
 <script>
 import TheHeader from "./components/views/TheHeader.vue";
+import AvailableJobs from "./components/AvailableJobs.vue";
+import data from "./data.json";
 export default {
-  components: { TheHeader },
+  data() {
+    return {
+      allData: data,
+    };
+  },
+  components: { TheHeader, AvailableJobs },
 };
 </script>
 <style>
@@ -13,5 +29,13 @@ html,
 body {
   margin: 0;
   padding: 0;
+}
+.container {
+  background-color: #effafa;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 }
 </style>
