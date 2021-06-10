78eac5972e4b1e9c18eb3155577d5559563f936f
Afshar07 <mr.afshar0@gmail.com>
2021-06-10 14:37:34 +0430

    create TheHeader and add style for mobile design

---
+++ src/components/views/TheHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<style scoped>
+@media only screen and (max-width: 500px) {
+  body {
+    margin: 0;
+    padding: 0;
+    width: 100vw !important;
+  }
+  .header {
+    background: url("../../assets/bg-header-mobile.svg") no-repeat,
+      linear-gradient(to right, #5ba4a4 220px, #63baba 70%);
+    width: 100%;
+    height: 9.5rem;
+  }
+}
+</style>
