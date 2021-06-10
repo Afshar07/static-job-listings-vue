99c79f8b206bf63fe1d2d381fa2676b5a65358ff
Afshar07 <mr.afshar0@gmail.com>
2021-06-11 01:07:12 +0430

    Change font size and some details in styling

---
+++ src/components/badges/NewBadge.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="badge-container">
+    <p>NEW!</p>
+  </div>
+</template>
+<style scoped>
+.badge-container {
+  width: auto;
+  height: auto;
+  background-color: #5ba4a4;
+  border-radius: 20px;
+}
+p {
+  color: #fff;
+  font-weight: 700;
+  font-size: 0.5rem;
+  margin: 0.3rem 0.5rem;
+}
+</style>
