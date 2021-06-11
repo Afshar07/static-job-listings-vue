dfacf032597d00ce442e5b7a2ed6b2292d038a0c
Afshar07 <mr.afshar0@gmail.com>
2021-06-11 20:20:04 +0430

    Change the html structure and add flexbox

--- src/components/views/JobDescription.vue
+++ src/components/views/JobDescription.vue
@@ -1,11 +1,31 @@
 <template>
-    <ul>
-        <li>{{ desc }}</li>
+  <div>
+    <ul class="description-container">
+      <li class="job-desc" v-for="desc in description" :key="desc">
+        {{ desc }}
+      </li>
     </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    props:["desc"]
-}
+  props: ["description"],
+};
 </script>
+
+<style scoped>
+.description-container {
+  color: #7b8e8e;
+  display: flex;
+  padding: 0;
+}
+.job-desc {
+  font-size: 0.8rem;
+  margin-left: 1.5rem;
+}
+.job-desc:first-of-type {
+  margin-left: 0;
+  list-style-type: none;
+}
+</style>
