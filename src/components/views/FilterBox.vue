9c501ceef28ab2d7924dca7ce858bf6e658f03c4
Afshar07 <mr.afshar0@gmail.com>
2021-06-12 00:22:14 +0430

    Create FilterBox component

---
+++ src/components/views/FilterBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="filterbox">
+    <filter-badge
+      :isRemovable="isFilterRemovable"
+      v-for="filter in filtered"
+      :key="filter"
+      :filter="filter"
+    ></filter-badge>
+  </div>
+</template>
+
+<script>
+import FilterBadge from "../badges/FilterBadge.vue";
+export default {
+  components: { FilterBadge },
+  props: ["filtered"],
+  data() {
+    return {
+      isFilterRemovable: true,
+    };
+  },
+};
+</script>
+
+<style scoped>
+.filterbox {
+  background-color: #fff;
+  display: flex;
+}
+</style>
