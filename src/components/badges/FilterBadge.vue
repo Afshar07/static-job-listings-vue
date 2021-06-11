2a8ea3070ce72521c57966353c59c94836ccf85a
Afshar07 <mr.afshar0@gmail.com>
2021-06-12 00:20:40 +0430

    Change the emits and add img for removing

--- src/components/badges/FilterBadge.vue
+++ src/components/badges/FilterBadge.vue
@@ -1,26 +1,45 @@
 <template>
   <div class="badge-container">
     <p class="badge-text">{{ filter }}</p>
+    <div class="remove" v-if="isRemovable">
+      <img :src="removeIcon" alt="remove Icon" class="remove-icon" />
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["filter"],
+  props: ["filter", "isRemovable"],
+  emits: ["filtered"],
+  data() {
+    return {
+      removeIcon: require("../../assets/icon-remove.svg"),
+    };
+  },
 };
 </script>
 
 <style scoped>
 .badge-container {
   background-color: rgb(239, 250, 250);
-  padding: 0.4rem 0.5rem;
   margin: 0.5rem;
-  cursor: pointer;
+  display: flex;
+  border-radius: 4px;
 }
 .badge-text {
   color: #5ba4a4;
   font-weight: 700;
   font-size: 0.7rem;
-  margin: 0;
+  margin: 0.4rem 0.5rem;
+}
+.remove {
+  margin-left: 0.5rem;
+  height: auto;
+  display: flex;
+  align-items: center;
+  padding: 0.4rem;
+  background-color: #5ba4a4;
+  border-top-right-radius: 4px;
+  border-bottom-right-radius: 4px;
 }
 </style>
