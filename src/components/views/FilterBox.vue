be9ae3781bb8b3c7b571eb0d3247015109f382a9
Afshar07 <mr.afshar0@gmail.com>
2021-06-13 16:20:08 +0430

    Add media query syntax

--- src/components/views/FilterBox.vue
+++ src/components/views/FilterBox.vue
@@ -34,26 +34,28 @@ export default {
 </script>
 
 <style scoped>
-.filterbox {
-  background-color: #fff;
-  width: 90%;
-  display: flex;
-  flex-wrap: wrap;
-  justify-content: space-around;
-  border-radius: 10px;
-  margin-top: -3rem;
-  margin-bottom: 1rem;
-}
-.badges {
-  flex-basis: 70%;
-  display: flex;
-  flex-wrap: wrap;
-}
-.clear-btn {
-  background: none;
-  border: none;
-  color: #5ba4a4;
-  font-weight: 700;
-  font-size: .9rem;
+@media only screen and (max-width: 500px) {
+  .filterbox {
+    background-color: #fff;
+    width: 90%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    border-radius: 10px;
+    margin-top: -3rem;
+    margin-bottom: 1rem;
+  }
+  .badges {
+    flex-basis: 70%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .clear-btn {
+    background: none;
+    border: none;
+    color: #5ba4a4;
+    font-weight: 700;
+    font-size: 0.9rem;
+  }
 }
 </style>
