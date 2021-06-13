eb039bfa4c75cd84adc8941205ef859d1bf1edab
Afshar07 <mr.afshar0@gmail.com>
2021-06-13 16:19:12 +0430

    Add more style to FilterBox component

--- src/components/views/FilterBox.vue
+++ src/components/views/FilterBox.vue
@@ -1,12 +1,16 @@
 <template>
   <div class="filterbox">
-    <filter-badge
-      :isRemovable="isFilterRemovable"
-      v-for="filter in filtered"
-      :key="filter.id"
-      :filter="filter"
-      @remove-filter="removeFilter"
-    ></filter-badge>
+    <!-- <div class="spacer"></div> -->
+    <div class="badges">
+      <filter-badge
+        :isRemovable="isFilterRemovable"
+        v-for="filter in filtered"
+        :key="filter.id"
+        :filter="filter"
+        @remove-filter="removeFilter"
+      ></filter-badge>
+    </div>
+    <button class="clear-btn">Clear</button>
   </div>
 </template>
 
@@ -32,7 +36,24 @@ export default {
 <style scoped>
 .filterbox {
   background-color: #fff;
+  width: 90%;
   display: flex;
   flex-wrap: wrap;
+  justify-content: space-around;
+  border-radius: 10px;
+  margin-top: -3rem;
+  margin-bottom: 1rem;
+}
+.badges {
+  flex-basis: 70%;
+  display: flex;
+  flex-wrap: wrap;
+}
+.clear-btn {
+  background: none;
+  border: none;
+  color: #5ba4a4;
+  font-weight: 700;
+  font-size: .9rem;
 }
 </style>
