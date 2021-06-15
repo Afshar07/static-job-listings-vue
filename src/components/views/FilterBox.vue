e305040e31e5364059c2f1679079c558d9945037
Afshar07 <mr.afshar0@gmail.com>
2021-06-15 15:59:58 +0430

    Add desktop styles and isFilterBox props for css styles inside the filterBadge cmp

--- src/components/views/FilterBox.vue
+++ src/components/views/FilterBox.vue
@@ -3,6 +3,7 @@
     <div class="badges">
       <filter-badge
         :isRemovable="isFilterRemovable"
+        :isFilterBox="isFilterBox"
         v-for="filter in filtered"
         :key="filter.id"
         :filter="filter"
@@ -22,6 +23,7 @@ export default {
   data() {
     return {
       isFilterRemovable: true,
+      isFilterBox: true,
     };
   },
   methods: {
@@ -58,6 +60,37 @@ export default {
     color: #7b8e8e;
     font-weight: 700;
     font-size: 0.9rem;
+  }
+}
+@media only screen and (min-width: 501px) {
+  .filterbox {
+    background-color: #fff;
+    width: 80%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    border-radius: 10px;
+    margin-top: -1.5rem;
+    margin-bottom: 1rem;
+    box-shadow: 0px 10px 15px -3px rgba(0, 0, 0, 0.1);
+    padding: 0.4rem;
+  }
+  .badges {
+    flex-basis: 70%;
+    display: flex;
+    flex-wrap: wrap;
+  }
+  .clear-btn {
+    background: none;
+    border: none;
+    color: #7b8e8e;
+    font-weight: 700;
+    font-size: 0.9rem;
+    cursor: pointer;
+  }
+  .clear-btn:hover {
+    color: #5ba4a4;
+    text-decoration: underline;
   }
 }
 </style>
