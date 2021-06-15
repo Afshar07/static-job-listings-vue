05560aa7997fef687e0bd4ac66b27eae6f93f103
Afshar07 <mr.afshar0@gmail.com>
2021-06-15 16:00:28 +0430

    Recieve the isFilterBox props and add desktop styles accordingly

--- src/components/badges/FilterBadge.vue
+++ src/components/badges/FilterBadge.vue
@@ -1,15 +1,18 @@
 <template>
-  <div class="badge-container">
+  <div
+    class="badge-container"
+    :class="{ badgeContainerFilterbox: isFilterBox }"
+  >
     <p class="badge-text">{{ filter }}</p>
-    <div class="remove" v-if="isRemovable">
-      <img :src="removeIcon" alt="remove Icon" @click="removeFilter" />
+    <div class="remove" v-if="isRemovable" @click="removeFilter">
+      <img :src="removeIcon" alt="remove Icon" class="remove-icon" />
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ["filter", "isRemovable"],
+  props: ["filter", "isRemovable", "isFilterBox"],
   emits: ["remove-filter"],
   data() {
     return {
@@ -58,6 +61,8 @@ export default {
     border-radius: 4px;
     margin: 0.5rem;
     cursor: pointer;
+    display: flex;
+    align-items: center;
   }
   .badge-container :hover {
     background-color: #5ba4a4;
@@ -65,12 +70,34 @@ export default {
     border-radius: 4px;
   }
 
+  .badgeContainerFilterbox :hover {
+    background-color: #effafa !important;
+    color: #5ba4a4;
+  }
+
   .badge-text {
     color: #5ba4a4;
     font-weight: 700;
     font-size: 0.7rem;
-    padding: .35rem .5rem;
+    padding: 0.35rem 0.5rem;
     margin: 0;
+  }
+  .remove {
+    height: auto;
+    display: flex;
+    align-items: center;
+    padding: 0.4rem;
+    background-color: #5ba4a4;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
+  .remove-icon:hover {
+    background: none !important;
+  }
+  .remove:hover {
+    border-top-left-radius: 0 !important;
+    border-bottom-left-radius: 0 !important;
+    background-color: #2c3a3a !important;
   }
 }
 </style>
