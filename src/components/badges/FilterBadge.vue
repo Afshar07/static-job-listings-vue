2cd40648948fe39e481b76c5ddc4f3d78f316c32
Afshar07 <mr.afshar0@gmail.com>
2021-06-15 15:16:12 +0430

    Add the desktop styles for hover state

--- src/components/badges/FilterBadge.vue
+++ src/components/badges/FilterBadge.vue
@@ -25,26 +25,52 @@ export default {
 </script>
 
 <style scoped>
-.badge-container {
-  background-color: rgb(239, 250, 250);
-  margin: 0.5rem;
-  display: flex;
-  border-radius: 4px;
+@media only screen and (max-width: 500px) {
+  .badge-container {
+    background-color: #effafa;
+    margin: 0.5rem;
+    display: flex;
+    border-radius: 4px;
+    min-width: 1rem;
+    min-height: 1rem;
+  }
+
+  .badge-text {
+    color: #5ba4a4;
+    font-weight: 700;
+    font-size: 0.7rem;
+    margin: 0.4rem 0.5rem;
+  }
+  .remove {
+    margin-left: 0.5rem;
+    height: auto;
+    display: flex;
+    align-items: center;
+    padding: 0.4rem;
+    background-color: #5ba4a4;
+    border-top-right-radius: 4px;
+    border-bottom-right-radius: 4px;
+  }
 }
-.badge-text {
-  color: #5ba4a4;
-  font-weight: 700;
-  font-size: 0.7rem;
-  margin: 0.4rem 0.5rem;
-}
-.remove {
-  margin-left: 0.5rem;
-  height: auto;
-  display: flex;
-  align-items: center;
-  padding: 0.4rem;
-  background-color: #5ba4a4;
-  border-top-right-radius: 4px;
-  border-bottom-right-radius: 4px;
+@media only screen and (min-width: 501px) {
+  .badge-container {
+    background-color: #effafa;
+    border-radius: 4px;
+    margin: 0.5rem;
+    cursor: pointer;
+  }
+  .badge-container :hover {
+    background-color: #5ba4a4;
+    color: #fff;
+    border-radius: 4px;
+  }
+
+  .badge-text {
+    color: #5ba4a4;
+    font-weight: 700;
+    font-size: 0.7rem;
+    padding: .35rem .5rem;
+    margin: 0;
+  }
 }
 </style>
