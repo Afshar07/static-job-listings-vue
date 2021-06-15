29977f878246bf202f46f3a2f0926e0419b361c1
Afshar07 <mr.afshar0@gmail.com>
2021-06-15 13:59:26 +0430

    Add header styles for desktop

--- src/components/views/TheHeader.vue
+++ src/components/views/TheHeader.vue
@@ -3,17 +3,24 @@
 </template>
 
 <style scoped>
+body {
+  margin: 0;
+  padding: 0;
+  width: 100vw !important;
+}
 @media only screen and (max-width: 500px) {
-  body {
-    margin: 0;
-    padding: 0;
-    width: 100vw !important;
-  }
   .header {
     background: url("../../assets/bg-header-mobile.svg") no-repeat,
       linear-gradient(to right, #5ba4a4 220px, #63baba 70%);
     width: 100%;
     height: 9.5rem;
+  }
+}
+@media only screen and (min-width: 501px) {
+  .header {
+    background: #5ba4a4 url("../../assets/bg-header-desktop.svg") no-repeat;
+    width: 100%;
+    height: 9.8rem;
   }
 }
 </style>
